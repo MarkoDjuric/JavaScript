146b12eebb0c91d923e62245fbc7eefbb90ddad8
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-10-14 20:35:57 +0200

    Update map_exercises.js

--- Functional JavaScript/map_exercises.js
+++ Functional JavaScript/map_exercises.js
@@ -2,8 +2,16 @@
 const start = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
-var reversed = (start) => start.map((_, index, array) => start[start.length-1 - index])
+const reversed = (start) => start.map((_, index, array) => start[start.length-1 - index])
 
 
-var reversed_second = (start) => start.map(el => el).reverse();
+const reversed_second = (start) => start.map(el => el).reverse();
 // expected output: Array [10, 9, 8, 7, 6, 5, 4
+
+
+// 2. Write a map function to print the Job: Name:
+const shipMates = [["Mal", "Captain"], ["Wash", "Pilot"], ["Zoey", "1st Mate"], ["Jayne", "Public Relations"]]
+
+// your code
+const jobNames = (shipMates) => shipMates.map((el,index) => el[1])
+// expected output: Array ["Captain: Mal", etc...]
