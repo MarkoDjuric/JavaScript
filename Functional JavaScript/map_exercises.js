19cd0ad0c6e9f876145f63b6e8e0e0523b535530
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-11-27 20:24:14 +0100

    Update map_exercises.js

--- Functional JavaScript/map_exercises.js
+++ Functional JavaScript/map_exercises.js
@@ -15,3 +15,32 @@ const shipMates = [["Mal", "Captain"], ["Wash", "Pilot"], ["Zoey", "1st Mate"], 
 // your code
 const jobNames = (shipMates) => shipMates.map((el,index) => el[1])
 // expected output: Array ["Captain: Mal", etc...]
+
+// 3. Use Object.keys with the map method to print "This character has a(n) [property-name]" for each property
+
+const character = {
+    id: '12mn18udcbv9823',
+    name: 'Chewbacca',
+    race: 'Wookie',
+    planet: 'Kashyyyk',
+    job: 'First Mate'
+};
+
+console.log(Object.keys(character));
+
+// Expected Output:
+// This character has a(n) id.
+// This character has a(n) name.
+// This character has a(n) race.
+// This character has a(n) planet.
+// This character has a(n) job.
+// [ 'This character has a(n) id',  'This character has a(n) name',  'This character has a(n) race', 'This character has a(n) planet', 'This character has a(n) job'
+
+
+
+Object.keys(character).map(key => {
+    console.log(`This character has a(n) ${key}`)
+    return `This character has a(n) ${key}`
+})
+
+
