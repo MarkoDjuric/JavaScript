c3f4a1c1ef6202719d3d87bc256cedf1538f7c81
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-11-29 23:30:47 +0100

    Update map_exercises.js

--- Functional JavaScript/map_exercises.js
+++ Functional JavaScript/map_exercises.js
@@ -108,3 +108,52 @@ const words = [12, 13, 14, 15, 16, 17]
 // Your Code
 words.filter(el => el % 2 == 0)
 // expected output: Array [12, 14, 16]
+
+
+// REAL LIFE EXAMPLE
+// We often use filter to quickly pull all the items that share a status or other characteristic. For instance,
+// create a list of all the active bounty hunters from the array below:
+
+const hunters = [
+    {
+        name: 'Greedo',
+        universe: 'Star Wars',
+        status: 'active',
+    },
+    {
+        name: 'Boba Fett',
+        universe: 'Star Wars',
+        status: 'inactive',
+    },
+    {
+        name: 'Asajj Ventress',
+        universe: 'Star Wars',
+        status: 'unknown',
+    },
+    {
+        name: 'Zam Wesell',
+        universe: 'Star Wars',
+        status: 'inactive',
+    },
+    {
+        name: 'Jango Fett',
+        universe: 'Star Wars',
+        status: 'active',
+    },
+]
+
+// Your code
+hunters.filter(el => el.status == 'active')
+
+// expected output: Array [
+//     {
+//         name: 'Greedo',
+//         universe: 'Star Wars',
+//         status: 'active',
+//     },
+//     {
+//         name: 'Jango Fett',
+//         universe: 'Star Wars',
+//         status: 'active',
+//     },
+// ]
