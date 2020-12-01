8cb854c5b7f559ad913f6d59b162e144c99e1edd
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-12-01 12:33:25 +0100

    Update helper_methods_exercisses.js

--- Functional JavaScript/helper_methods_exercisses.js
+++ Functional JavaScript/helper_methods_exercisses.js
@@ -13,3 +13,22 @@ const characters = [
 ]
 
 const results = characters.flat()
+
+
+// ----------------------------------------------------------
+// FIND EXERCISE
+// ----------------------------------------------------------
+// Best use cases for FIND are when you want to cast a wider net, because you get to create your own criteria that can be either very specific or more generic.
+
+// Determine whether any of the following have a value that contains the characters 'ABC'
+
+const ids = [
+    'ADHKE',
+    'ANFKM',
+    'QIMVU',
+    'PQMFU',
+    'ABCKO',
+    'IUABC'
+]
+
+var findStr = ids.find(el => el.includes("ABC"))
