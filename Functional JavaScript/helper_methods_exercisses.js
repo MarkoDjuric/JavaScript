795b7adeb0f694e76dde9993241892c6a2788f52
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-12-01 12:33:03 +0100

    Update helper_methods_exercisses.js

--- Functional JavaScript/helper_methods_exercisses.js
+++ Functional JavaScript/helper_methods_exercisses.js
@@ -1,1 +1,15 @@
 
+// FLAT EXERCISE
+// ----------------------------------------------------------
+// This short list of some marvel characters is abitrarily nested. Experiment with the effect of flattening to various depths.
+
+
+const characters = [
+    ['Starlord', 'Gamora', 'Groot'],
+    ['Dr. Strange', ['Captain America', 'Bucky Barnes'], ['Thor', 'Hulk', ['Loki']], 'Thanos']
+    ['Iron Man', 'Ultron'],
+    ['Spider Man', ['Venom']],
+    ['Professor X', 'Wolverine', 'Quicksilver', ['Magneto']]
+]
+
+const results = characters.flat()
